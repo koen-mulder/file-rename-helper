bac5d1c5213d89556c296f9f0053fb2b901eb587
Koen Mulder <koenmldr@gmail.com>
2025-02-02 02:20:44 +0100

    Use SL4J with Log4j2

--- src/main/java/main/java/Application.java
+++ src/main/java/main/java/Application.java
@@ -8,13 +8,14 @@ import java.awt.EventQueue;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 
 public class Application {
 
     // Create a logger instance
-    private static final Logger logger = LogManager.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     /**
      * Launch the application.
